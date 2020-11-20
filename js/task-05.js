1e13eb590d1ab5ad8c9b149860854b7c57f4a3ab
sergiuszorgo <67199252+sergiuszorgo@users.noreply.github.com>
2020-11-20 14:02:17 +0200

    Задание 5, 6

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,17 @@
 const inputLine = document.querySelector('#name-input');
 const nameLine = document.querySelector('#name-output');
 
-inputLine.addEventListener('input', e => {
-    nameLine.textContent = e.target.value;
+// inputLine.addEventListener('input', event => {
+//     nameLine.textContent = event.target.value;
+// });
+
+// Исправил: теперь при пустом инпуте - в спане отображается слово "незнакомец"
+
+inputLine.addEventListener('input', event => {
+    if (inputLine.value.length == 0) {
+        nameLine.textContent = 'незнакомец';
+    }
+    else {
+        nameLine.textContent = event.target.value;
+    }
 });
--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,21 @@
 const inputValid = document.querySelector('#validation-input');
 
+// inputValid.addEventListener('blur', () => {
+//     inputValid.value.length === Number(inputValid.getAttribute("data-length")) ?
+//         inputValid.classList.add("valid") : inputValid.classList.add("invalid")
+// });
+
+// Сделал как просили без тернарного
+
 inputValid.addEventListener('blur', () => {
-    inputValid.value.length === Number(inputValid.getAttribute("data-length")) ?
-        inputValid.classList.add("valid") : inputValid.classList.add("invalid")
+    if (inputValid.value.length === Number(inputValid.getAttribute("data-length"))) {
+        inputValid.classList.add("valid");
+    }
+    else {
+        inputValid.classList.add("invalid");
+    }
 });
+
 inputValid.addEventListener('focus', () => {
     inputValid.classList.remove("valid")
     inputValid.classList.remove("invalid")
